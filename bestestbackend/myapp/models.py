47ff69a8370cc77f2518095dad7e043900f31723
girindradmada <girindradaafi@gmail.com>
2025-05-18 14:22:45 +0700

    added admin panel for mixmodelrequest, used for posting job details

--- bestestbackend/myapp/models.py
+++ bestestbackend/myapp/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
+
+class OrderStatus(models.TextChoices):
+    PENDING = 'Pending', 'Pending' # Ternyata value pertama itu value external dan yang value kedua yang internal server 
+    ACCEPTED = 'Accepted', 'Accepted' # Absolutely kreizi
+    REJECTED = 'Rejected', 'Rejected'
+    @classmethod 
+    def ColorChoices(cls, status):
+        colorMap = {
+           cls.PENDING: '#FFDE21',
+           cls.ACCEPTED: '#56D47A',
+           cls.REJECTED: '#FF3F41'
+       }
+        return colorMap.get(status, '#FFDE21')
+    
+class MixModelRequest(models.Model):
+    # Auto increment number of orders
+    orderName = models.AutoField(primary_key=True)
+
+    # Status of job
+    status = models.CharField(
+        max_length = 10,
+        choices = OrderStatus.choices,
+        default = OrderStatus.PENDING.name,
+    )
+
+    # Name of job poster
+    solicitor = models.CharField(max_length=100)
+
+    # Date deadline for order
+    date = models.DateField()
+
+    # Duration of time until deadline
+    @property
+    def duration(self):
+        delta = (self.date - now().date()).days
+        return max(delta, 0)
+
+    # Price of job
+    price = models.IntegerField()
+
+    # Color of status
+    @property
+    def status_color(self):
+        return OrderStatus.ColorChoices(self.status)
+
+    def __str__(self):
+        return f"Order: {self.orderName} - {self.status} - By: {self.solicitor} - Due in: {self.duration} at {self.date} - {self.price} - Status Color: {self.status_color}"
+    
--- bestestbackend/myapp/tests.py
+++ bestestbackend/myapp/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django.utils.timezone import now
+from .models import MixModelRequests
+from datetime import date, timedelta
+
 
 # Create your tests here.
